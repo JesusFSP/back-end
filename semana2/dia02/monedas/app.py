c2b41bb7316bdf82eb44135523fd4cbd24559ec1
JesusFSP <jsilva@unsa.edu.pe>
2023-07-27 02:28:48 -0500

    Creando un convertidor de monedas

---
+++ semana2/dia02/monedas/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+TITULO_PAGINA = "TIPO DE CAMBIO DE LA SBS"
+@app.route('/')
+def index():
+    
+    return render_template('index.html', titulo = TITULO_PAGINA)
+
+@app.route('/convertir', methods=['POST'])
+def convertir():
+    if request.method == "POST":
+        monto_soles = request.form['soles']
+        resultado = round(float(monto_soles) / 3.588, 2)
+        
+    return render_template('index.html', titulo = TITULO_PAGINA, monto=resultado)
+app.run(debug=True)
